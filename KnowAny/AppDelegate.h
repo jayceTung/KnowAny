234346ba681a569e8ca098b46d5efa0e924c13f8
Super <minchao.dong@melot.cn>
2018-04-19 18:52:19 +0800

    Initial Commit

---
+++ KnowAny/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KnowAny
+//
+//  Created by super on 2018/4/19.
+//  Copyright © 2018年 super. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KnowAny/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KnowAny
+//
+//  Created by super on 2018/4/19.
+//  Copyright © 2018年 super. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
